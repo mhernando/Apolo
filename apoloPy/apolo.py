f38d2f80b4d798cae71af559c26de24967aea3d4
Miguel Hernando <miguel.hernando@upm.es>
2017-10-31 12:24:58 +0100

    python API
    finished... need to be tested

--- apoloPy/apolo.py
+++ apoloPy/apolo.py
@@ -32,6 +32,19 @@ def apolo_message(code):
             return bytearray(data)
         return function_wrapper
     return message_decorator
+def extractData(data):
+    if len(data)<5 or data[0:2] != b'aa':
+        return None
+    size = int.from_bytes(data[2:4],byteorder='little')
+    if data[4]==ord(AP_TRUE):
+        return True
+    elif data[4]==ord(AP_FALSE):
+        return False
+    elif data[4]==ord(AP_DVECTOR):
+        tam = int.from_bytes(data[5:7],byteorder='little')
+        if(tam*8+7>len(data)):
+            print("BAD FORMATED MESSAGE")
+        return [x[0] for x in struct.iter_unpack('d',data[7:])]
 
 def writeData(data, *args):
     for thing in args:
@@ -57,18 +70,18 @@ def m_setRobotJoints(data,robot,values,world=''):
     writeData(data, world, robot,Uint8(len(values)),f_values)
 
 @apolo_message(AP_PLACE)
-def m_placeObject(data,world, object, x,y,z,roll,pitch,yaw):
+def m_placeObject(data, object, x,y,z,roll,pitch,yaw, world = ''):
     writeData(data, world, object,
               float(x), float(y),float(z),
               float(roll), float(pitch),float(yaw))
 
 @apolo_message(AP_MOVE_WB)
-def m_moveWheeledBase(data,world, robot, speed, rotspeed, time):
+def m_moveWheeledBase(data, robot, speed, rotspeed, time, world = ''):
     writeData(data, world, robot,
               float(speed), float(rotspeed), float(time))
 
 @apolo_message(AP_PLACE_WB)
-def m_placeWheeledBase(data,world, robot,  x,y,z,yaw):
+def m_placeWheeledBase(data, robot,  x,y,z,yaw,world = ''):
     writeData(data, world, robot,
               float(x), float(y),float(z),float(yaw))
 @apolo_message(AP_CHECKJOINTS)
@@ -101,19 +114,39 @@ class Apolo:
         self.data=[]
 
     def updateWorld(self,world=''):
-        m=m_updateWorld(self.data,world)
-        self.sock.send(m)
+        self.sock.send(m_updateWorld(self.data,world))
 
     def setRobotJoints(self,robot,values,world=''):
-        m=m_setRobotJoints(self.data, robot, values, world)
-        self.sock.send(m)
+        self.sock.send(m_setRobotJoints(self.data, robot, values, world))
+
+    def checkRobotJoints(self,robot, values, world=''):
+        self.sock.send(m_checkJoints(self.data, robot, values, world))
+        return extractData(self.sock.recv(1024))
+
+    def getLocation(self, object, world=''):
+        self.sock.send(m_getLocation(self.data, object, world))
+        return extractData(self.sock.recv(1024))
+
+    def moveWheeledBase(self, robot, speed, rotspeed, time, world=''):
+        self.sock.send(m_moveWheeledBase(self.data, robot, speed, rotspeed, time, world))
+
+    def placeWheeledBase(self, robot, x,y,z,yaw, world=''):
+        self.sock.send(m_placeWheeledBase(self.data, robot, x,y,z,yaw,world))
+        return extractData(self.sock.recv(1024))
+
+    def getLocationWB(self, robot, world=''):
+        self.sock.send(m_getLocationWB(self.data, robot, world))
+        return extractData(self.sock.recv(1024))
+
+    def linkToRobotTCP(self, robot, object, world=''):
+        self.sock.send(m_linkToRobotTCP(self.data, robot, object, world))
 
 if __name__ == "__main__":
     import time
     ap=Apolo()
     ap.updateWorld()
-    ap.setRobotJoints("ASEA",[0.5, 1, 0.5])
+    ap.checkRobotJoints("Puma 560",[0.5, 1, 0.5])
     for i in range(-100,100):
-        ap.setRobotJoints("ASEA",[i*0.01, i*0.02, i*0.01])
-        ap.updateWorld()
-        time.sleep(0.02)
+        print(ap.checkRobotJoints("Puma 560",[i*0.01, i*0.03, i*0.01]))
+        #ap.updateWorld()
+    print(ap.getLocation('Puma 560'))
